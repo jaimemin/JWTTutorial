ec054350844aab52bbc5de0a63e25b4b15750cca
jaimemin <jaimemin@gmail.com>
2022-05-01 23:40:54 +0900

    Initial commit

---
+++ src/test/java/com/tisory/jaimemin/jwt/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tisory.jaimemin.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
